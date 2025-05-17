90c3a5172f46d842f283a56998e0b3de1c318b4e
rnk7112517 <rnk7112517@gmail.com>
2025-05-17 23:59:00 +0900

    検索後に検索ワードを画面に表示済み

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -3,6 +3,12 @@
     <form action="" method="get">
         <input type="search" name="search" placeholder="ユーザ名">
         <input type="image" src="/images/search.png"  width="30" height="30">
+
+        {{-- 検索ワードの表示（検索された時だけ） --}}
+    @if(!empty($search))
+        <span style="margin-left: 130px;">検索ワード: {{ $search }}</span>
+    @endif
+
     </form>
 
     <!-- 検索結果の表示 -->
@@ -10,7 +16,7 @@
         @if($users->isNotEmpty())
             <ul>
                 @foreach($users as $user)
-                    <li>{{ $user->username }}</li>
+                <li>{{ $user->username }}</li>
                     <img src="{{ asset('images/'. $user->icon_image) }}" alt="アイコン" >
                 @endforeach
             </ul>
